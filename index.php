3cdec3ccc8a8db71c5c544a1ebab5307ea35e287
Santosh <g.santosh.sunny@gmail.com>
2017-09-19 19:23:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,6 @@
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Binteractive Co.</title>
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-	  <script src="https://use.fontawesome.com/815eddff59.js"></script>  <!-- icons -->
-                                                                        <!-- fonts -->
-    <link rel="icon" type="image/png" href="images/favicon.png">        <!-- logo -->
-  </head>
-  <body>
-    <header>
-
-      <!-- header -->
-
-        <nav class="navbar fixed">
-          <span class="navbar-text">
-          <i class="fa fa-phone" aria-hidden="true"> </i> +1 111 222 3333
-          </span>
-
-
-          <div class="social-icon">
-            <a href="#" class="social-button twitter"><i class="fa fa-twitter"></i></a>
-            <a href="#" class="social-button facebook"><i class="fa fa-facebook"></i></a>
-            <a href="#" class="social-button google"><i class="fa fa-google-plus"></i></a>
-            <a href="#" class="social-button skype"><i class="fa fa-skype"></i></a>
-          </div>
-
-
-        </nav>
-
-
+<?php
+  include('include/header.php');
+?>
 
         <div id="carousel" class="carousel slide" data-ride="carousel">
               <ol class="carousel-indicators">
