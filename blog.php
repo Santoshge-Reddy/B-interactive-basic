048c301bacdf2ef2cbaf46b927d88e5cec8e4ea5
Santosh <g.santosh.sunny@gmail.com>
2017-10-27 20:59:55 +0530

    Create blog.php
    blog

---
+++ blog.php
@@ -0,0 +1,22 @@
+<?php
+  include('include/header.php');
+?>
+
+<img class="d-block w-100" src="image/default.jpg" height="300px" alt="default">
+
+<?php
+  include('include/navbar.php');
+?>
+
+</header>
+
+    <div class="main container">
+      <section>
+        <h1>Blog</h1>
+        <iframe src="https://binteractiveco.wordpress.com/" width="100%" height="2000" frameborder="0" scrolling="yes"></iframe>
+      </section>
+    </div>
+
+<?php
+  include('include/footer.php');
+?>
